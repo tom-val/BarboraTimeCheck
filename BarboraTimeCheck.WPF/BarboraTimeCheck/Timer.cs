f19b0fe5b18aeaae31c1a6e7dd2c6ffc08ca0494
AzureAD\TomasValiunas <tomas.valiunas@devbridge.com>
2020-03-23 14:41:19 +0200

    Show last check information

--- BarboraTimeCheck.WPF/BarboraTimeCheck/Timer.cs
+++ BarboraTimeCheck.WPF/BarboraTimeCheck/Timer.cs
@@ -1,5 +1,7 @@
 ﻿using BarboraTimeCheck.Services;
 using Notifications.Wpf;
+using System;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading;
@@ -9,6 +11,44 @@ namespace BarboraTimeCheck
 {
     public static class TimerManager
     {
+        private static string _lastCheckString = "Not checked yet";
+
+        public static string LastCheck
+        {
+            get
+            {
+                return _lastCheckString;
+            }
+
+            set
+            {
+                _lastCheckString = value;
+                OnStaticPropertyChanged("LastCheck");
+            }
+        }
+
+        private static int _deliveriesFound = 0;
+
+        public static int DeliveriesFound
+        {
+            get
+            {
+                return _deliveriesFound;
+            }
+
+            set
+            {
+                _deliveriesFound = value;
+                OnStaticPropertyChanged("DeliveriesFound");
+            }
+        }
+
+        public static event EventHandler<PropertyChangedEventArgs> StaticPropertyChanged = delegate { };
+        private static void OnStaticPropertyChanged(string staticPropertyName)
+        {
+            StaticPropertyChanged(null, new PropertyChangedEventArgs(staticPropertyName));
+        }
+
         private static Timer gAppTimer;
         private static object lockObject = new object();
         private static SettingsService configurationService = new SettingsService();
@@ -54,6 +94,8 @@ namespace BarboraTimeCheck
         {
             if (configurationService.AuthenticationExists())
             {
+                LastCheck = DateTime.Now.ToString();
+
                 var settings = configurationService.GetSettings();
                 var deliveries = barboraService.GetAvailableDeliveries();
 
@@ -61,6 +103,8 @@ namespace BarboraTimeCheck
                 {
                     return;
                 }
+
+                DeliveriesFound = deliveries.Count;
 
                 var deliveryText = new StringBuilder();
                 deliveryText.AppendLine("Available delivery times:");
